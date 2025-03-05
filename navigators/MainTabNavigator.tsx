660cf6ad47082322a43cc173ab8901d1970e02f9
HanseKim <104922418+HanseKim@users.noreply.github.com>
2025-03-05 15:15:49 +0900

    Add files via upload

--- navigators/MainTabNavigator.tsx
+++ navigators/MainTabNavigator.tsx
@@ -1,119 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-
-import {
-  Image,
-  SafeAreaView,
-  Text,
-} from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
-import {PreDiaryQuestionsScreen }from '../screens/WriteDiaryScreen';
+import { PreDiaryQuestionsScreen } from '../screens/WriteDiaryScreen';
 import ChatScreen from '../screens/ChatScreen';
 import MyInfoScreen from '../screens/MyInfoScreen';
+import DiaryDetailScreen from '../screens/DiaryDetail';
+import MessageScreen from '../screens/MessageScreen';
+import { WriteDiaryScreen } from '../screens/WriteDiaryScreen';
+import EditDiaryScreen from '../screens/EditDiaryScreen';
 
-type mainTabParamList = {
-    Home : undefined,
-    Search : undefined,
-    Write : undefined,
-    Chat : undefined,
-    MyInfo : undefined
-  };
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const BottomTab = createBottomTabNavigator<mainTabParamList>();
+// 각 탭에 대한 스택 네비게이터
+const HomeStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Screen name="Detail" component={DiaryDetailScreen} />
+    <Stack.Screen name="EditDiaryScreen" component={EditDiaryScreen} />
+  </Stack.Navigator>
+);
+
+const ChatStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="ChatScreen" component={ChatScreen} />
+    <Stack.Screen name="Message" component={MessageScreen} />
+  </Stack.Navigator>
+);
+
+const WriteStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="PreDiaryQuestions" component={PreDiaryQuestionsScreen} />
+    <Stack.Screen name="WriteDiaryScreen" component={WriteDiaryScreen} />
+  </Stack.Navigator>
+);
+
+// const icons = {
+//   Home: require('../images/Homebtn.png'),
+//   Search: require('../images/Search.png'),
+//   Write: require('../images/Diarybtn.png'),
+//   Chat: require('../images/Chatbtn.png'),
+//   MyInfo: require('../images/Mypagebtn.png'),
+// };
+
+const icons: { [key: string]: any } = {
+  Home: require('../images/Homebtn.png'),
+  Search: require('../images/Search.png'),
+  Write: require('../images/Diarybtn.png'),
+  Chat: require('../images/Chatbtn.png'),
+  MyInfo: require('../images/Mypagebtn.png'),
+};
 
 function MainTabNavigator(): React.JSX.Element {
-
   return (
-      <BottomTab.Navigator
-        screenOptions={{
-          headerShown: false, 
-          tabBarActiveTintColor: "#F5BFD9", // 활성화 상태의 텍스트 및 아이콘 색상
-          tabBarInactiveTintColor: "#999", // 비활성화 상태의 텍스트 및 아이콘 색상
-          tabBarStyle: { backgroundColor: "#fff", height: "8%" }, // 탭 바 배경색, 높이
-      }}>
-        <BottomTab.Screen 
-          name="Home"
-          component={HomeScreen} 
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("../images/Homebtn.png") // 선택된 이미지
-                    : require("../images/Homebtn.png") // 비선택 이미지
-                }
-                style={{ width: 24, height: 24 }} // 색상 적용
-              />
-            ),}}></BottomTab.Screen>
-        <BottomTab.Screen 
-          name="Search" 
-          component={SearchScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("../images/Search.png") // 선택된 이미지
-                    : require("../images/Search.png") // 비선택 이미지
-                }
-                style={{ width: 24, height: 24 }}
-              />
-            ),}}></BottomTab.Screen>
-        <BottomTab.Screen 
-          name="Write" 
-          component={PreDiaryQuestionsScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("../images/Diarybtn.png") // 선택된 이미지
-                    : require("../images/Diarybtn.png") // 비선택 이미지
-                }
-                style={{ width: 24, height: 24 }}
-              />
-            ),}}></BottomTab.Screen>
-        <BottomTab.Screen 
-          name="Chat"
-          component={ChatScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("../images/Chatbtn.png") // 선택된 이미지
-                    : require("../images/Chatbtn.png") // 비선택 이미지
-                }
-                style={{ width: 24, height: 24 }}
-              />
-            ),}}></BottomTab.Screen>
-        <BottomTab.Screen 
-          name="MyInfo" 
-          component={MyInfoScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("../images/Mypagebtn.png") // 선택된 이미지
-                    : require("../images/Mypagebtn.png") // 비선택 이미지
-                }
-                style={{ width: 24, height: 24 }}
-              />
-            ),}}></BottomTab.Screen>
-      </BottomTab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: '#F5BFD9',
+        tabBarInactiveTintColor: '#999',
+        tabBarStyle: styles.tabBarStyle,
+        tabBarIcon: ({ focused }) => (
+          <Image
+            source={icons[route.name]}
+            style={styles.icon}
+          />
+        ),
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Write" component={WriteStack} />
+      <Tab.Screen name="Chat" component={ChatStack} />
+      <Tab.Screen name="MyInfo" component={MyInfoScreen} />
+    </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: '#fff',
+    height: 80, // % 대신 px로 고정
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
 
 export default MainTabNavigator;
